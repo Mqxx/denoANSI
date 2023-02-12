f6025a26d6a8972dd7d809aff8f0b774c4963832
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-02-12 21:33:29 +0100

    Updated files

--- src/constants/codes.ts
+++ src/constants/codes.ts
@@ -1,10 +1,63 @@
+/**
+ * ANSI codes
+ * @enum {string}
+ */
+
 export enum codes {
-    BEL = '\x07', // Terminal bell
-    BS = '\x08', // Backspace
-    HT = '\x09', // Horizontal TAB
-    LF = '\x0A', // Linefeed (newline)
-    VT = '\x0B', // Vertical TAB
-    FF = '\x0C', // Formfeed
-    CR = '\x0D', // Carriage return (linestart)
-    DEL = '\x7F' // Delete character
+    /**
+     * `\x07` | `\a`
+     * 
+     * @desc Terminal bell
+     */
+    BEL = '\x07',
+    /**
+     * `\x08` | `\b`
+     * 
+     * @desc Backspace
+     */
+    BS = '\x08',
+    /**
+     * `\x09` | `\t`
+     * 
+     * @desc Horizontal TAB
+     */
+    HT = '\x09',
+    /**
+     * `\x0A` | `\n`
+     * 
+     * @desc Linefeed (newline)
+     */
+    LF = '\x0A',
+    /**
+     * `\x0B` | `\v`
+     * 
+     * @desc Vertical TAB
+     */
+    VT = '\x0B',
+    /**
+     * `\x0C` | `\f`
+     * 
+     * @desc Formfeed
+     */
+    FF = '\x0C',
+    /**
+     * `\x0D` | `\r`
+     * 
+     * @desc Carriage return (linestart)
+     */
+    CR = '\x0D',
+    /**
+     * `\x1B` | `\e*`
+     * 
+     * <i>*not guaranteed to work<i>
+     * 
+     * @desc Escape character
+     */
+    ESC = '\x1B',
+    /**
+     * `\x7F`
+     * 
+     * @desc Delete character
+     */
+    DEL = '\x7F'
 }
--- src/constants/sequences.ts
+++ src/constants/sequences.ts
@@ -1,8 +1,23 @@
 export enum sequences {
-    ESC = '\x1B',
+    /**
+     * Sequence start (Escape)
+     */
+    ESC = '\x1B',   
+    /**
+     * Control Sequence Introducer
+     */
     CSI = '\x1B[',
+    /**
+     * Device Control String
+     */
     DCS = '\x1BP',
+    /**
+     * Operating System Command
+     */
     OSC = '\x1B]',
+    /**
+     * Separator
+     */
     SEP = ';'
 
 }
