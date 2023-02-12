ae135b0c328fc563549caded132a155daaf6acdb
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-02-12 22:15:11 +0100

    Update erase.ts

---
+++ src/display/erase.ts
@@ -0,0 +1,31 @@
+
+
+export const erase = {
+    displayTo: function() : void {
+
+    },
+    displayToEnd: function() : void {
+
+    },
+    displayToStart: function() : void {
+
+    },
+    displayAll: function() : void {
+
+    },
+    lineSaved: function() : void {
+
+    },
+    lineTo: function() : void {
+        
+    },
+    lineToEnd: function() : void {
+
+    },
+    lineToStart: function() : void {
+
+    },
+    lineAll: function() : void {
+
+    }
+}
