f436224cff0131d2e0ffe7b1869cd706abc6407b
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-02-16 21:31:12 +0100

    Update grayscaleBG.ts

--- src/constants/colors/8bit/grayscaleBG.ts
+++ src/constants/colors/8bit/grayscaleBG.ts
@@ -1,177 +1,177 @@
 /**
- * `<CSI>` `38;5;{hex 1};{hex 2};{hex n}m`
+ * `<CSI>` `48;5;{hex 1};{hex 2};{hex n}m`
  * 
  * @desc ANSI 8-Bit background colors grayscale
  * @readonly
  * @enum {string}
  */
-export enum grayscaleFG {
+export enum grayscaleBG {
 
     /**
-     * `<CSI>` `38;5;232`
+     * `<CSI>` `48;5;232`
      * 
      * @desc Background grayscale #080808
      */
-    X080808 = '38;5;232',
+    X080808 = '48;5;232',
 
     /**
-     * `<CSI>` `38;5;233`
+     * `<CSI>` `48;5;233`
      * 
      * @desc Background grayscale #121212
      */
-    X121212 = '38;5;233',
+    X121212 = '48;5;233',
 
     /**
-     * `<CSI>` `38;5;234`
+     * `<CSI>` `48;5;234`
      * 
      * @desc Background grayscale #1C1C1C
      */
-    X1C1C1C = '38;5;234',
+    X1C1C1C = '48;5;234',
 
     /**
-     * `<CSI>` `38;5;235`
+     * `<CSI>` `48;5;235`
      * 
      * @desc Background grayscale #262626
      */
-    X262626 = '38;5;235',
+    X262626 = '48;5;235',
 
     /**
-     * `<CSI>` `38;5;236`
+     * `<CSI>` `48;5;236`
      * 
      * @desc Background grayscale #303030
      */
-    X303030 = '38;5;236',
+    X303030 = '48;5;236',
 
     /**
-     * `<CSI>` `38;5;237`
+     * `<CSI>` `48;5;237`
      * 
      * @desc Background grayscale #3A3A3A
      */
-    X3A3A3A = '38;5;237',
+    X3A3A3A = '48;5;237',
 
     /**
-     * `<CSI>` `38;5;238`
+     * `<CSI>` `48;5;238`
      * 
      * @desc Background grayscale #444444
      */
-    X444444 = '38;5;238',
+    X444444 = '48;5;238',
 
     /**
-     * `<CSI>` `38;5;239`
+     * `<CSI>` `48;5;239`
      * 
      * @desc Background grayscale #4E4E4E
      */
-    X4E4E4E = '38;5;239',
+    X4E4E4E = '48;5;239',
 
     /**
-     * `<CSI>` `38;5;240`
+     * `<CSI>` `48;5;240`
      * 
      * @desc Background grayscale #585858
      */
-    X585858 = '38;5;240',
+    X585858 = '48;5;240',
 
     /**
-     * `<CSI>` `38;5;241`
+     * `<CSI>` `48;5;241`
      * 
      * @desc Background grayscale #626262
      */
-    X626262 = '38;5;241',
+    X626262 = '48;5;241',
 
     /**
-     * `<CSI>` `38;5;242`
+     * `<CSI>` `48;5;242`
      * 
      * @desc Background grayscale #6C6C6C
      */
-    X6C6C6C = '38;5;242',
+    X6C6C6C = '48;5;242',
 
     /**
-     * `<CSI>` `38;5;243`
+     * `<CSI>` `48;5;243`
      * 
      * @desc Background grayscale #767676
      */
-    X767676 = '38;5;243',
+    X767676 = '48;5;243',
 
     /**
-     * `<CSI>` `38;5;244`
+     * `<CSI>` `48;5;244`
      * 
      * @desc Background grayscale #808080
      */
-    X808080 = '38;5;244',
+    X808080 = '48;5;244',
 
     /**
-     * `<CSI>` `38;5;245`
+     * `<CSI>` `48;5;245`
      * 
      * @desc Background grayscale #8A8A8A
      */
-    X8A8A8A = '38;5;245',
+    X8A8A8A = '48;5;245',
 
     /**
-     * `<CSI>` `38;5;246`
+     * `<CSI>` `48;5;246`
      * 
      * @desc Background grayscale #949494
      */
-    X949494 = '38;5;246',
+    X949494 = '48;5;246',
 
     /**
-     * `<CSI>` `38;5;247`
+     * `<CSI>` `48;5;247`
      * 
      * @desc Background grayscale #9E9E9E
      */
-    X9E9E9E = '38;5;247',
+    X9E9E9E = '48;5;247',
 
     /**
-     * `<CSI>` `38;5;248`
+     * `<CSI>` `48;5;248`
      * 
      * @desc Background grayscale #A8A8A8
      */
-    XA8A8A8 = '38;5;248',
+    XA8A8A8 = '48;5;248',
 
     /**
-     * `<CSI>` `38;5;249`
+     * `<CSI>` `48;5;249`
      * 
      * @desc Background grayscale #B2B2B2
      */
-    XB2B2B2 = '38;5;249',
+    XB2B2B2 = '48;5;249',
 
     /**
-     * `<CSI>` `38;5;250`
+     * `<CSI>` `48;5;250`
      * 
      * @desc Background grayscale #BCBCBC
      */
-    XBCBCBC = '38;5;250',
+    XBCBCBC = '48;5;250',
 
     /**
-     * `<CSI>` `38;5;251`
+     * `<CSI>` `48;5;251`
      * 
      * @desc Background grayscale #C6C6C6
      */
-    XC6C6C6 = '38;5;251',
+    XC6C6C6 = '48;5;251',
 
     /**
-     * `<CSI>` `38;5;252`
+     * `<CSI>` `48;5;252`
      * 
      * @desc Background grayscale #D0D0D0
      */
-    XD0D0D0 = '38;5;252',
+    XD0D0D0 = '48;5;252',
 
     /**
-     * `<CSI>` `38;5;253`
+     * `<CSI>` `48;5;253`
      * 
      * @desc Background grayscale #DADADA
      */
-    XDADADA = '38;5;253',
+    XDADADA = '48;5;253',
 
     /**
-     * `<CSI>` `38;5;254`
+     * `<CSI>` `48;5;254`
      * 
      * @desc Background grayscale #E4E4E4
      */
-    XE4E4E4 = '38;5;254',
+    XE4E4E4 = '48;5;254',
 
     /**
-     * `<CSI>` `38;5;255`
+     * `<CSI>` `48;5;255`
      * 
      * @desc Background grayscale #EEEEEE
      */
-    XEEEEEE = '38;5;255'
+    XEEEEEE = '48;5;255'
 }
