d403e1fa79d4a9c7e0aa64db04fec5e9a1a99426
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-02-16 19:54:57 +0100

    Create colors8BrightBG.ts

---
+++ src/constants/colors/colors8BrightBG.ts
@@ -0,0 +1,78 @@
+/**
+ * `<CSI>` `{color 1};{color 2};{color n}m`
+ * 
+ * @desc ANSI background 8 colors bright
+ * @readonly
+ * @enum {string}
+ */
+export enum colors8BrightBG {
+    /**
+     * `<CSI>` `0`
+     * 
+     * @desc Reset bright background
+     */
+    RESET = '0',
+
+    /**
+     * `<CSI>` `100`
+     * 
+     * @desc Bright black background
+     */
+    BRIGHT_BLACK = '100',
+
+    /**
+     * `<CSI>` `101`
+     * 
+     * @desc Bright red background
+     */
+    BRIGHT_RED = '101',
+
+    /**
+     * `<CSI>` `102`
+     * 
+     * @desc Bright green background
+     */
+    BRIGHT_GREEN = '102',
+
+    /**
+     * `<CSI>` `103`
+     * 
+     * @desc Bright yellow background
+     */
+    BRIGHT_YELLOW = '103',
+
+    /**
+     * `<CSI>` `104`
+     * 
+     * @desc Bright blue background
+     */
+    BRIGHT_BLUE = '104',
+
+    /**
+     * `<CSI>` `105`
+     * 
+     * @desc Bright magenta background
+     */
+    BRIGHT_MAGENTA = '105',
+
+    /**
+     * `<CSI>` `106`
+     * 
+     * @desc Bright cyan background
+     */
+    BRIGHT_CYAN = '106',
+
+    /**
+     * `<CSI>` `107`
+     * 
+     * @desc Bright white background
+     */
+    BRIGHT_WHITG = '107',
+
+    /**
+     * `<CSI>` `39`
+     * 
+     * @desc Default background
+     */
+    DEFAULT = '39',
+}
