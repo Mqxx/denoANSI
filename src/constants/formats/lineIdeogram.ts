22501b9c496455b1a040e8248b10b37f37f4bbff
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-02-18 21:34:16 +0100

    Create lineIdeogram.ts

---
+++ src/constants/formats/lineIdeogram.ts
@@ -0,0 +1,44 @@
+/**
+ * ANSI formats
+ * 
+ * @desc ANSI text format codes
+ * @readonly
+ * @enum {string}
+ */
+export enum lineIdeogram {
+    /**
+     * @desc Line ideogram right side format
+     * @default formats.RIGHT_SIDE = '60'
+     */
+    RIGHT_SIDE = '60',
+
+    /**
+     * @desc Line ideogram double right side format
+     * @default formats.DOUBLE_RIGHT_SIDE = '61'
+     */
+    DOUBLE_RIGHT_SIDE = '61',
+
+    /**
+     * @desc Line ideogram left side format
+     * @default formats.LEFT_SIDE = '62'
+     */
+    LEFT_SIDE = '62',
+
+    /**
+     * @desc Line ideogram double left side format
+     * @default formats.DOUBLE_LEFT_SIDE = '63'
+     */
+    DOUBLE_LEFT_SIDE = '63',
+
+    /**
+     * @desc Line ideogram stress marking format
+     * @default formats.STRESS_MARKING = '64'
+     */
+    STRESS_MARKING = '64',
+
+    /**
+     * @desc Reset ideogram format
+     * @default formats.RESET = '65'
+     */
+    RESET = '65'
+}
