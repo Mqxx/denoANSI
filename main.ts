00856698a0e043fae8bcefce55d6bb79a380482d
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-02-19 17:32:56 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-import { cursor } from "./src/cursor/cursor.ts";
 
-await cursor.savePosition()
-await cursor.moveTo({row: 100, column: 500})
-// await cursor.moveLeft(1)
-// await cursor.moveUp(1)
-console.log('#');
-await cursor.restorePosition()
